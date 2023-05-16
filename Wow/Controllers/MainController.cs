5b31723abfce62685b853ea503eb6c0149e9276f
RagalikAlik <nzavilenski@gmail.com>
2023-05-16 14:38:33 +0300

    11

---
+++ Wow/Controllers/MainController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace Wow.Controllers
+{
+    public class MainController
+    {
+        static IConfigurationBuilder builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true);
+
+        static readonly IConfiguration Configuration = builder.Build();
+
+        private static readonly string? Link = Configuration.GetConnectionString("DefaultConnection");
+
+        [HttpPost]
+        public static string GetAmountOfUsers()
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(Link))
+            {
+                conn.ConnectionString = Link;
+
+                conn.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand();
+
+                cmd.CommandText = $"SELECT count(*) FROM users";
+                cmd.Connection = conn;
+                return cmd.ExecuteScalar().ToString();
+            }
+        }
+
+    }
+}
